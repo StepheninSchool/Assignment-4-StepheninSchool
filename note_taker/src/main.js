2142c48a8d84bbf2f47d4861c7383523aef31ef9
StepheninSchool <w0488750@campus.nscc.ca>
2025-03-25 21:03:19 -0300

    Refactor App.vue and HelloWorld.vue for improved layout and note management; add noteStore for state management with Pinia

--- note_taker/src/main.js
+++ note_taker/src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
+// import pinia to use the store    - Stephen L 
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import 'bulma/css/bulma.css' // Import Bulma CSS 8:25pm Stephen L
 
-createApp(App).mount('#app')
+// Create the Vue app instance - Stephen L
+const app = createApp(App)
 
+// Use Pinia for state management - Stephen L
+app.use(createPinia())
+
+// Mount the app to the DOM - Stephen L
+app.mount('#app')
