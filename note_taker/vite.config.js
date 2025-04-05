1a67217d5ac0f8ac1e1b7d410e05c644aa04735a
StepheninSchool <w0488750@campus.nscc.ca>
2025-04-04 21:34:41 -0300

    added vite-plugin-pwa for offline access. Configured in vite.config.js

--- note_taker/vite.config.js
+++ note_taker/vite.config.js
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Note Taker',
+        short_name: 'Notes',
+        description: 'Offline Notes App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/app-development.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/app-development2.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+          },
+          {
+            urlPattern: ({ request }) =>
+              ['style', 'script', 'worker'].includes(request.destination),
+            handler: 'CacheFirst',
+          },
+        ],
+      },
+    }),
+  ],
+});
+  
+
