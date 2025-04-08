f8a476f6ae5afa5fa6098bd8ae6994f14299bb4c
StepheninSchool <w0488750@campus.nscc.ca>
2025-04-07 23:23:08 -0300

    Remove unused style.css import from main.js

--- note_taker/src/main.js
+++ note_taker/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia'; // import pinia to use the store    - Stephen L 
 import App from './App.vue';
 import router from './router';
-import './style.css';
+
 import 'bulma/css/bulma.min.css'; // Import Bulma CSS 8:25pm Stephen L
 
 // Create the Vue app instance - Stephen L
